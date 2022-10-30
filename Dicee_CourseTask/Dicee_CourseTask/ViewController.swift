159deca414c3422c16b64b786860d687a5c47873
Alexander <a.pride.xxi@tutanota.com>
2022-10-30 13:40:05 +0300

    Initial Commit

---
+++ Dicee_CourseTask/Dicee_CourseTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dicee_CourseTask
+//
+//  Created by Александр Прайд on 30.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
